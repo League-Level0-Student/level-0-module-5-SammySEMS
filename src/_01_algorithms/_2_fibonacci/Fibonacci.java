0850b7b4d8d5d0b10dd477edd0a6f33b6a7bede6
root <root@edf14d5156dc>
2021-04-19 20:27:59 +0000

    GoofyName

--- src/_01_algorithms/_2_fibonacci/Fibonacci.java
+++ src/_01_algorithms/_2_fibonacci/Fibonacci.java
@@ -1,23 +1,23 @@
 package _01_algorithms._2_fibonacci;
 
 public class Fibonacci {
-public static void main(String[] args) {
-	int number1;
-	int number2;
-	int sum; 
-	
-	
-	number1 = 0;
-	number2 = 1;
+	public static void main(String[] args) {
 
-	System.out.println(number1);
-	System.out.println(number2);
-	
-	for ( int i = 0; i < 11; i++) {
-		System.out.println();
+		int number1 = 0;
+		int number2 = 1;
+		int sum;
+
+		System.out.println(number1);
+		System.out.println(number2);
+
+		for (int i = 0; i < 20; i++) {
+			sum = number1 + number2;
+			number1 = number2;
+			number2 = sum;
+
+			System.out.println(number2);
+
+		}
 	}
-	
-	
-	
-}
+
 }
--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -11,29 +11,34 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String name = JOptionPane.showInputDialog("Please enter your name");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-
+		// Run your program to see that this works.
+		System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 7).
-		//    HINT: Use .length() to determine the number of characters in the String.
+		// HINT: Use .length() to determine the number of characters in the String.
+		for (int i = 0; i < name.length(); i++) {
 
-				// 4. Create a char variable to store the next character of the name
-				//    use .charAt()
-	
-				
-				// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
-			
-			
-				// 6. Even characters should be made uppercase and odd characters made lowercase
-				//    HINT: use Character.toUpperCase() or Character.toLowerCase()
-	
-			
-				// 7. ADD the char to the end of the goofyName String
+			// 4. Create a char variable to store the next character of the name
+			// use .charAt()
+			char c = name.charAt(i);
 
-		
+			// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
+
+			if (i % 2 == 0) {
+				goofyName += Character.toUpperCase(c);
+			} else {
+				goofyName += Character.toLowerCase(c);
+
+			}
+		}
+
+		// 6. Even characters should be made uppercase and odd characters made lowercase
+		// HINT: use Character.toUpperCase() or Character.toLowerCase()
+
+		// 7. ADD the char to the end of the goofyName String
+
 		// 8. Use pop-up to show user their Goofy name
-
+JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
-
