6b7ab4cbb094ad09ff8250ae89565cc730e00a50
root <root@16424db850fb>
2021-04-26 20:27:16 +0000

    Amazing Rings

--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -2,54 +2,60 @@ package _02_nested_loops._1_snowflake;
 
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class Snowflake {
-	
+
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
-
+		Robot r2d2 = new Robot();
 
 		// #2. Set your robot’s position to x=300 and y=300
-
-
+		r2d2.setX(300);
+		r2d2.setY(300);
 		// #3. Put the robot's pen down
-
+		r2d2.penDown();
 
 		// #4. Set the robot’s speed to 5
+		r2d2.setSpeed(5);
 
+		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6
+		// times (use i as the counter)
+		for (int i = 0; i < 6; i++) {
 
-		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
-
-			
 			// #6. Set the pen color to black
+			r2d2.setPenColor(Color.black);
 
-			
 			// #7. move the robot by 50
+			r2d2.move(50);
 
-			
 			// #8. turn the robot to the right by 60
-			
-			
-			// #12. set the pen color to blue for even numbers of i
+			r2d2.turn(60);
 
-			
+			// #12. set the pen color to blue for even numbers of i
+			if ((i % 2) == 0) {
+				r2d2.setPenColor(Color.blue);
+			}
+
 			// #13. set the pen color to red for all odd numbers of i
-			
-			
+			else {
+				r2d2.setPenColor(Color.red);
+			}
+
 			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
-			
-			
-					// #10. turn the robot to the left by 60
-		
-					
-					// #11. move the robot by 50 (play the start button)
-					
-				
-			
+			for (int j = 0; j < 6; j++) {
+
+				// #10. turn the robot to the left by 60
+				r2d2.turn(-60);
+
+				// #11. move the robot by 50 (play the start button)
+				r2d2.move(50);
+			}
+
 			// #14. set the pen color to black
-				
-		
+			r2d2.setPenColor(Color.black);
+		}
 	}
 
 }
--- src/_02_nested_loops/_2_amazing_rings/amazing_rings/amazing_rings.pde
+++ src/_02_nested_loops/_2_amazing_rings/amazing_rings/amazing_rings.pde
@@ -1,27 +1,43 @@
+int x1 = 800;
+int x2 = 200;
+int y = 250;
 
   /*** Go to the recipe to run the demonstration before starting this program ***/
 
 void setup() {
   /* Set the size of your sketch to be a rectangle like in the recipe demonstration */
-  
+  size(1000,500);
   /* Call the noFill() command so all the ellipses will be transparent */
+  noFill();
+
+ 
 }
 
 void draw() {
-
+ int size = 350;
         /* Use a for loop to make the first set of rings that will start in the left half of the window
         (you can use the code from your Bullseye program).*/          
-
-        
+  for (int i = 0; i < 7; i++) {
+   ellipse(x1, y, size, size);
+   size-=50;
+  }
+  
+  size = 350;
+  for (int j = 0; j<8; j++){
+    ellipse(x2, y, size, size);
+    size-=50;
+  }
         /*Make this set of rings move across the sketch to the right 
         Hint: make two variables, one for x and another for the speed. 
         Then increase x by the amount in speed */
+x1--;
+x2++;
  
-        
+ //x= x+speed; 
+     // x+speed;  
         /*When the rings reach the right side of the sketch, reverse the direction so they move
         Hint: speed = -speed */
 
-         
         /*When the rings reach the left side of the sketch, reverse the direction again */
         
          
