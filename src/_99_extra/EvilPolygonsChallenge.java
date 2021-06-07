9f554730f58d44e55d2845b15ff3eb81dee03aa7
student <student@ip-172-31-49-137.us-west-2.compute.internal>
2021-06-07 13:21:23 -0700

    EvilPolygons

--- src/_99_extra/EvilPolygonsChallenge.java
+++ src/_99_extra/EvilPolygonsChallenge.java
@@ -10,13 +10,21 @@ public class EvilPolygonsChallenge {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-
+Robot rob = new Robot();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
+rob.setSpeed(100);
+		int ColorChoice =JOptionPane.showOptionDialog(null, "hello, choose a color", "person", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color Red","Color Green", "Color Blue"}, 0);
+		String colorchoice = JOptionPane.showInputDialog(null, "Red green or blue?");
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
+		if (colorChoice.equalsIgnoreCase("red")) {
+			rob.setPenColor(Color.red);
+		}
+		else if (colorChoice.equalsIgnoreCase("green")) {
+			rob.setPenColor(Color.green);
+		}
+		else {
+			rob.setPenColor(Color.blue);
+		}
 		//4. Ask the use how many polygons they want to be drawn.
 		
 		//5. Use the robot to draw the number of polygons the user requested.
